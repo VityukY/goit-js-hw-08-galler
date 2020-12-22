b9681d95ab748f840ca6fc017c753ae0aa36c2d5
Vityuk Yuriy <67076356+VityukY@users.noreply.github.com>
2020-12-22 17:41:19 +0200

    добавляет элементы галлери по шаблону

--- gallery.js
+++ gallery.js
@@ -1,3 +1,25 @@
 import galleryItem from './gallery-items.js'
-console.dir (galleryItem)
 
+const galleryRef = document.querySelector('.js-gallery')
+
+
+const galleryItemHandler = function ({preview,original,description}) {
+    const gallery_item = document.createElement('li');
+    gallery_item.classList.add('gallery__item');
+    const gallery_link = document.createElement('a');
+    gallery_link.classList.add('gallery__link');
+    const gallery_img = document.createElement('img');
+    gallery_img.classList.add('gallery__image');
+    gallery_img.dataset.source=original;
+    gallery_img.src =preview;
+    gallery_img.alt = description;
+    gallery_link.href = gallery_img.dataset.source;
+    gallery_link.appendChild(gallery_img);
+    gallery_item.appendChild(gallery_link)
+    return gallery_item;
+} 
+
+const arrayAddingPicture = galleryItem.map(item =>{
+    return galleryItemHandler(item)
+})
+galleryRef.append(...arrayAddingPicture)
