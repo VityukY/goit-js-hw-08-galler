40df0ae8343b462c72309a5e82fc507aaa756d92
Vityuk Yuriy <you.thunderstruck@gmail.com>
2020-12-25 18:57:19 +0200

    добавляет функционал модального окна
    - передача изображения в модальное окно
    - закрытие модального окна
    -очистка срц
    -закрытие по ескейпу
    - закрытие по фону

--- gallery.js
+++ gallery.js
@@ -1,8 +1,20 @@
 import galleryItem from './gallery-items.js'
 
 const galleryRef = document.querySelector('.js-gallery')
+const lightBoxImgRef = document.querySelector('.lightbox__image')
+const modalWindowRef = document.querySelector('.js-lightbox')
+const closeModalBtnRef = document.querySelector('.lightbox__button')
 
+const openModalhandler = function (){
+    modalWindowRef.classList.add('is-open')
 
+}
+const closeModalHandler = function () {
+    modalWindowRef.classList.remove('is-open');
+}
+const srcModalCleaner = function (){
+    lightBoxImgRef.src='';
+}
 const galleryItemHandler = function ({preview,original,description}) {
     const gallery_item = document.createElement('li');
     gallery_item.classList.add('gallery__item');
@@ -23,3 +35,31 @@ const arrayAddingPicture = galleryItem.map(item =>{
     return galleryItemHandler(item)
 })
 galleryRef.append(...arrayAddingPicture)
+
+
+closeModalBtnRef.addEventListener('click', ()=>{
+    closeModalHandler();
+    srcModalCleaner();
+})
+
+galleryRef.addEventListener('click', (event) => {
+ event.preventDefault()
+if (event.target.nodeName === 'IMG'){
+    lightBoxImgRef.src=event.target.dataset.source;
+    lightBoxImgRef.alt = event.target.alt;
+    openModalhandler()
+}})
+
+modalWindowRef.addEventListener('click', (event) => {
+    if (event.target.nodeName !== 'IMG'){
+        closeModalHandler()
+        srcModalCleaner()
+    }
+})
+
+window.addEventListener('keydown', (event) => {
+    if(event.code === 'Escape'){
+        closeModalHandler()
+        srcModalCleaner()
+    }
+})
